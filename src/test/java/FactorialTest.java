5012d991e3183b8edaec5a0ddfc1e31a698e6fc3
Abhigna <54097148+alpineFrost@users.noreply.github.com>
2023-03-21 22:19:38 +0530

    Changing test cases

--- src/test/java/FactorialTest.java
+++ src/test/java/FactorialTest.java
@@ -2,7 +2,6 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-
 public class FactorialTest {
     @BeforeClass
     public static void before_test() {
@@ -11,12 +10,16 @@ public class FactorialTest {
         System.out.println("starting factorial test");
         System.out.println("===============================================");
     }
-
-
     @Test
     public void positive_float() {
         Calculator mycalc = new Calculator();
         System.out.println("test 1) factorial +ve");
         assertEquals("factorial 5 not working correctly",120,mycalc.factorial(5),0.00001);
+    }
+    @Test
+    public void negative_float() {
+        Calculator mycalc = new Calculator();
+        System.out.println("test 2) factorial -ve");
+        assertEquals("factorial -5 not working correctly",-1,mycalc.factorial(-6),0.00001);
     }
 }
