c861c3277dc09285f9d5d0ea3c4dffe642e487a8
Abhigna <54097148+alpineFrost@users.noreply.github.com>
2023-03-21 21:27:45 +0530

    Update LogTest.java

--- src/test/java/LogTest.java
+++ src/test/java/LogTest.java
@@ -19,11 +19,4 @@ public class LogTest {
         System.out.println("test 1) log +ve");
         assertEquals("log 10 not working correctly",2.3025851,mycalc.nat_log(10),0.00001);
     }
-
-    @Test
-    public void negative_float() {
-        Calculator mycalc = new Calculator();
-        System.out.println("test 2) log -ve");
-        assertEquals("log -10 not working correctly",-1,mycalc.nat_log(-6),0.00001);
-    }
 }
