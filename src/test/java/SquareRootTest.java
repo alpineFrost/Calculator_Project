f3c5f08f98728f0355c1c8ebc620f748f3f5a6b0
Abhigna <54097148+alpineFrost@users.noreply.github.com>
2023-03-21 22:21:21 +0530

    Added test cases

--- src/test/java/SquareRootTest.java
+++ src/test/java/SquareRootTest.java
@@ -17,4 +17,11 @@ public class SquareRootTest {
         System.out.println("test 1) square root +ve");
         assertEquals("square root 25 not working correctly",5,mycalc.square_root(25),0.00001);
     }
+
+    @Test
+    public void negative_float() {
+        Calculator mycalc = new Calculator();
+        System.out.println("test 2) square root -ve");
+        assertEquals("square root -5 not working correctly",-1,mycalc.square_root(-6),0.00001);
+    }
 }
